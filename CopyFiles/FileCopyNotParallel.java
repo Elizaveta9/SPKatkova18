e751f49bf33445345380ffcc2a35a84e943e3a4c
Елизавета <katkova_lizochka@bk.ru>
2021-10-28 11:27:29 +0300

    Убрано использование nio

--- CopyFiles/FileCopyNotParallel.java
+++ CopyFiles/FileCopyNotParallel.java
@@ -1,29 +1,43 @@
 package com.company;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
 
 public class FileCopyNotParallel {
     public static void main(String[] args) {
         // Назначение источника и конечного пути копирования первого файла
-        File sourceFirst = new File("C:\\Users\\Elizaveta\\IdeaProjects\\ProjectForFunzis\\src\\com\\company\\FirstFileToCopy.txt");
+        File sourceFirst = new File("C:\\Users\\Elizaveta\\IdeaProjects\\title\\src\\com\\company\\files\\tagsREADABLE.txt");
         File destFirst = new File("C:\\Users\\Elizaveta\\IdeaProjects\\ProjectForFunzis\\src\\com\\company\\FirstFileToCopy2.txt");
 
         //Назначение источника и конечного пути копирования второго файла
-        File sourceSecond = new File("C:\\Users\\Elizaveta\\IdeaProjects\\ProjectForFunzis\\src\\com\\company\\SecondFileToCopy.txt");
+        File sourceSecond = new File("C:\\Users\\Elizaveta\\IdeaProjects\\title\\src\\com\\company\\files\\ftgForAllTradeNames.txt");
         File destSecond = new File("C:\\Users\\Elizaveta\\IdeaProjects\\ProjectForFunzis\\src\\com\\company\\SecondFileToCopy2.txt");
+
+        String line = "";
 
         long startTime = System.currentTimeMillis();
 
-        try {
-            Files.copy(sourceFirst.toPath(), destFirst.toPath());
-            Files.copy(sourceSecond.toPath(), destSecond.toPath());
+        try (BufferedReader br1 = new BufferedReader(new FileReader(sourceFirst));
+             BufferedReader br2 = new BufferedReader(new FileReader(sourceSecond));
+             BufferedWriter bw1 = new BufferedWriter(new FileWriter(destFirst));
+             BufferedWriter bw2 = new BufferedWriter(new FileWriter(destSecond))) {
+
+            // копирование первого файла
+            while ((line = br1.readLine()) != null) {
+                bw1.write(line);
+            }
+
+            line = "";
+
+            // копирование второго файла
+            while ((line = br2.readLine()) != null) {
+                bw2.write(line);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         long endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime-startTime) + "ms");
+        System.out.println("Total execution time: " + (endTime - startTime) + "ms");
     }
 }
--- CopyFiles/FilesCopyParallel.java
+++ CopyFiles/FilesCopyParallel.java
@@ -1,17 +1,15 @@
 package com.company;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
 
 public class FilesCopyParallel {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // Назначение источника и конечного пути копирования первого файла
-        File sourceFirst = new File("C:\\Users\\Elizaveta\\IdeaProjects\\ProjectForFunzis\\src\\com\\company\\FirstFileToCopy.txt");
+        File sourceFirst = new File("C:\\Users\\Elizaveta\\IdeaProjects\\title\\src\\com\\company\\files\\tagsREADABLE.txt");
         File destFirst = new File("C:\\Users\\Elizaveta\\IdeaProjects\\ProjectForFunzis\\src\\com\\company\\FirstFileToCopy2.txt");
 
         //Назначение источника и конечного пути копирования второго файла
-        File sourceSecond = new File("C:\\Users\\Elizaveta\\IdeaProjects\\ProjectForFunzis\\src\\com\\company\\SecondFileToCopy.txt");
+        File sourceSecond = new File("C:\\Users\\Elizaveta\\IdeaProjects\\title\\src\\com\\company\\files\\ftgForAllTradeNames.txt");
         File destSecond = new File("C:\\Users\\Elizaveta\\IdeaProjects\\ProjectForFunzis\\src\\com\\company\\SecondFileToCopy2.txt");
 
         long startTime = System.currentTimeMillis();
@@ -31,22 +29,32 @@ public class FilesCopyParallel {
         }
 
         long endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime-startTime) + "ms");
+        System.out.println("Total execution time: " + (endTime - startTime) + "ms");
     }
 }
 
 //класс потока для выполнения копирования
-class FileCopyTread extends Thread{
+class FileCopyTread extends Thread {
     private File source;
     private File dest;
+    private String line = "";
 
-    FileCopyTread(File source, File dest){
+    FileCopyTread(File source, File dest) {
         this.source = source;
         this.dest = dest;
     }
-    public void run(){
-        try {
-            Files.copy(source.toPath(), dest.toPath());
+
+    public void run() {
+        try (BufferedReader br1 = new BufferedReader(new FileReader(source));
+             BufferedWriter bw1 = new BufferedWriter(new FileWriter(dest))) {
+
+            // копирование первого файла
+            while ((line = br1.readLine()) != null) {
+                bw1.write(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
